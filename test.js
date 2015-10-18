f31d7d3471990b17c9d8c850b8aa1f71acb6d4b0
John Otander <johnotander@gmail.com>
2015-10-17 20:24:57 -0600

    Use ava

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-'use strict'
+import test from 'ava'
+import getClassesFromHtml from './'
 
-var test = require('tape')
-var getClassesFromHtml = require('..')
-
-
-var html = '<body class="foo  bar">' +
+const html = '<body class="foo  bar">' +
   '<span title="title" class="\tfoobar\t baz">hi</span>' +
   '<h1 CLASS=\'another-class\'>Woot woot</h1>' +
 '</body>'
 
-test('get-classes-from-html', function (t) {
+test('get-classes-from-html', t => {
   t.plan(1)
 
-  t.deepEqual(
+  t.same(
     getClassesFromHtml(html),
     ['foo', 'bar', 'foobar', 'baz', 'another-class']
   )
